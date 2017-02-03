e9504d42b98e51d93c7d5d629485b18e995115e0
elias <elias.jm.garcia@gmail.com>
2017-02-03 13:11:50 -0600

    initial commit of files to the repo

---
+++ demo.py
@@ -0,0 +1,47 @@
+"""
+this program is meant to demonstrate the use of git as a version control
+system. The methods below are trivial by nature and may be blank for the
+purpose of said demonstration.
+
+author: elias garcia
+veresion: 3.2.17
+"""
+
+import arrow
+
+
+def list_example():
+    """
+    print a list of integers using range()
+    """
+
+
+def print_date_today():
+
+    """
+    This doesn't print right...
+    """
+    print("The date today is ".format(arrow.now().format("YYYY-MM-DD")))
+
+    # what about the format? don't we typically put year last?
+
+
+def print_names():
+    """
+    this prints names
+    """
+    print("Will, Austin, Isaac, and Elias.")
+
+
+def main():
+    """
+    main method
+    """
+
+    print("Hello, world! Here's a list of the names of our group!")
+    print_names()
+    print_date_today()
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+this program is meant to demonstrate the use of git as a version control
+system. The methods below are trivial by nature and may be blank for the
+purpose of said demonstration.
+
+author: elias garcia
+veresion: 3.2.17
+"""
+
+import arrow
+
+
+def print_date_today():
+
+    """
+    This doesn't print right...
+    """
+    print("The date today is".format(arrow.now()))
+
+
+def print_names():
+
+    print("Will, Austin, Isaac, and Elias.")
+
+
+def main():
+
+    print("Hello, world! Here's a list of the names of our group!")
+    print_names()
+    print_date_today()
+
+    
+if __name__ == "__main__":
+    main()
